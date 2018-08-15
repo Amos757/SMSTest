bf47c7b8ef6097a2d9b978fa834f9a07d74af392
AMOS CHUI HON YANG <17045722@rp.edu.sg>
2018-08-15 15:37:45 +0800

    Initial commit

---
+++ app/src/test/java/sg/edu/rp/c346/smstest/ExampleUnitTest.java
@@ -0,0 +1,13 @@
+package sg.edu.rp.c346.smstest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
